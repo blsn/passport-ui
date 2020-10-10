236ae68033c754026906315e4911a52089e1fa65
Moshe Balasiano <mbalasiano@leolane.com>
2020-10-11 00:39:03 +0300

    first commit

---
+++ database/seeders/TodosTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Todo;
+
+class TodosTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Todo::factory()->times(10)->create(['user_id' => 1]); 
+    }
+}
---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::factory()->create([
+            'email'     => 'user@user.com',
+            'password'  => Hash::make('12345678')
+        ]); 
+    }
+}
